0b78b1d677d15bb5094462bb20b421d51afa35ca
qianben <463006889@qq.com>
2018-09-12 16:48:44 +0800

    first commd

---
+++ src/main/java/com/qin/zsbp/ZsbpApplication.java
@@ -0,0 +1,12 @@
+package com.qin.zsbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZsbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZsbpApplication.class, args);
+	}
+}
